913c4c29eb4bdf44ad0f1ceebc0d996f1ad9a330
E Cho <helloecho12345@gmail.com>
2022-01-11 15:57:48 +0000

    bookmarks_manager updated with links in table and TablePlus installed

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,10 +1,14 @@
+require 'pg'
 
 class Bookmark
   def self.all
-    [
-      "http://www.youtube.com",
-      "http://www.imdb.com",
-      "http://www.google.com"
-    ]
+    connection = PG.connect(dbname: 'bookmark_manager')
+    result = connection.exec("SELECT * FROM bookmarks;")
+    result.map { |bookmark| bookmark['url'] }
+    # [
+    #   "http://www.youtube.com",
+    #   "http://www.imdb.com",
+    #   "http://www.google.com"
+    # ]
   end
 end
